03e8a138d976c62d64e9850607eda8c48ba9c983
Sharan AJ <sharanajs2001@gmail.com>
2021-12-05 18:36:19 +0530

    alphabet pattern completed

--- alphabet_pattern.cpp
+++ alphabet_pattern.cpp
@@ -38,11 +38,38 @@ void pattern3(int a) {
     }
     cout << endl;
 }
+
+void pattern4(int a) {
+    int alpha = 65;
+    cout << "pattern 4" << endl;
+    for(int i = 0;i < a;i++) {
+        for(int j = 0;j < (a-i);j++) {
+            printf("%c ",alpha);
+            alpha+=1;
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void pattern5(int a) {
+    int alpha = 65;
+    cout << "pattern 5" << endl;
+    for(int i = 0;i < a;i++) {
+        for(int j = 0;j <= i;j++) {
+            printf("%c ",alpha+i);
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
 int main() {
     int input=0;
     cin >> input;
     pattern1(input);
     pattern2(input);
     pattern3(input);
+    pattern4(input);
+    pattern5(input);
     return 0;
 }
