e718596238e3fb800bdcf9b65a662eaf97caaff2
ajs-codes <sharanajs2001@gmail.com>
2025-03-31 15:59:40 +0530

    removed all the existing code

--- java-basics/Hello.java
+++
@@ -1,5 +0,0 @@
-class Hello{
-    public static void main(String[] argv) {
-        System.out.println("hello world");
-    }
-}
--- java-basics/Input.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-class Input{
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String msg = input.nextLine();
-        System.out.println("input msg is : " + msg);
-    }
-}
