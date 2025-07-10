c34a93300fe0c5d5bcbd6619c663d546a2b377ba
ajs-codes <sharanajs2001@gmail.com>
2025-07-10 15:43:28 +0530

    Java setup

--- stivers_atoz_dsa/Main.java
+++ stivers_atoz_dsa/Main.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int x  = scanner.nextInt();
+        String x = scanner.nextLine();
         System.out.println("Hello Java");
         System.out.println(x);
-        scanner.close();
     }
 }
