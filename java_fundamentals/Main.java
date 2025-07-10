417d569a78411d7a111693c1916c7564e268394c
ajs-codes <sharanajs2001@gmail.com>
2025-07-10 15:53:24 +0530

    Simple java program

---
+++ java_fundamentals/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println("Hello Java");
+        add(x, y);
+        scanner.close();
+    }
+
+    public static void add(int x, int y) {
+        System.out.println(x + y);
+    }
+}
