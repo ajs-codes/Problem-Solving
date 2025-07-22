6afb426dfc166cfdf9641c99cbd483daf964f631
ajs-codes <sharanajs2001@gmail.com>
2025-07-22 12:58:49 +0530

    for each loop in java

--- java_fundamentals/Loops.java
+++ java_fundamentals/Loops.java
@@ -12,5 +12,15 @@ public class Loops {
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
         }
+
+        // For each loop
+        String[] arr = {"A", "B", "C", "D"};
+
+        for (String i : arr) {
+            System.out.print(i + " ");
+            if (i == arr[arr.length - 1]) {
+                System.out.println();
+            }
+        }
     }
 }
